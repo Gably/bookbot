7e77b90f9b8cf3b08d4fd8f9272878bd49ee559d
Gably <lysgabriel95@gmail.com>
2024-08-22 03:08:03 +0800

    added count_words function, took it out from main()

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ def main():
         file_contents = f.read()
     
     words = file_contents.split()
-    num_of_words = len(words)
-    print(num_of_words)
+    num_of_words = count_words(words)
+    print(f"Number of words: {num_of_words}")
+
+def count_words(words):
+    return len(words)
+
+
 
 main()
