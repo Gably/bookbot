1f49f0419d04eb875c65c19f380add757d6ad32d
Gably <lysgabriel95@gmail.com>
2024-08-22 03:15:00 +0800

    updated count_words function

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def main():
     with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
+        book = f.read()
     
-    words = file_contents.split()
-    num_of_words = count_words(words)
+    num_of_words = count_words(book)
     print(f"Number of words: {num_of_words}")
 
-def count_words(words):
+def count_words(book):
+    words = book.split()
     return len(words)
 
 
